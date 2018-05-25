17b9e7716aa6b3c4711e147847df72d1a944584f
opulinks <help@opulinks.com>
2018-05-25 19:09:19 +0800

    opl1000 sdk 1.0.1.18

---
+++ SDK/APS_PATCH/middleware/netlink/data_flow/data_flow_patch.h
@@ -0,0 +1,18 @@
+/******************************************************************************
+*  Copyright 2017 - 2018, Opulinks Technology Ltd.
+*  ---------------------------------------------------------------------------
+*  Statement:
+*  ----------
+*  This software is protected by Copyright and the information contained
+*  herein is confidential. The software may not be copied and the information
+*  contained herein may not be used or disclosed except with the written
+*  permission of Netlink Communication Corp. (C) 2017
+******************************************************************************/
+
+#ifndef __DATA_FLOW_PATCH_H__
+#define __DATA_FLOW_PATCH_H__
+
+int ipc_rf_at_process_M0ToM3_patch(void* pdata);
+
+#endif //#ifndef __DATA_FLOW_PATCH_H__
+
