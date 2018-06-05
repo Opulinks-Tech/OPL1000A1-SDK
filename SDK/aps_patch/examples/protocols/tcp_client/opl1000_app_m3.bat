3834fbbb8fa1a88b93cea70e6a2bc12f68c0f21c
opulinks <help@opulinks.com>
2018-06-05 14:20:29 +0800

    opl1000 sdk 1.0.1.19

--- SDK/aps_patch/examples/protocols/tcp_client/opl1000_app_m3.bat
+++
@@ -1,3 +0,0 @@
-c:\Keil_v5\ARM\ARMCC\bin\fromelf.exe Output\Objects\opl1000_app_m3.axf --m32 --output Output\Objects\opl1000_app_m3.s19
-c:\Keil_v5\ARM\ARMCC\bin\fromelf.exe Output\Objects\opl1000_app_m3.axf --bin --output Output\Objects\opl1000_app_m3.bin
-..\..\..\..\APS\tools\srec_cat.exe Output\Objects\opl1000_app_m3.bin -binary -byte-swap 4 -o Output\Objects\opl1000_app_m3_srecord.hex -vmem 32
